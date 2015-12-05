f210ebec0d8f75b3f65bfec62f51924c80bda907
D1o0g9s <boodalina@gmail.com>
2015-12-04 20:38:35 -0800

    Created DeadReckoningAutonomous
    I made the file and put a comment. Nothing interesting.

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DeadReckoningAutonomous.java
@@ -0,0 +1,16 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * This is the Dead Reckoning Autonomous program.
+ * It will drive to the beacon with the intake on,
+ * dump the climbers, and drive up the ramp. It
+ * uses IMU for turning and driving straight. It
+ * will also have continuous arm control because
+ * the arm is not stable.
+ *
+ * Created by glchau on 12/4/15.
+ */
+public class DeadReckoningAutonomous {
+
+
+}
