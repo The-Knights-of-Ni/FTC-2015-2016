50b92b264dd472b3b8e2d41b16380a0ae0d004af
kylerNat <kylerNat@gmail.com>
2015-12-06 19:17:12 -0800

    cleaned up ndk test

--- FtcRobotController/src/main/jni/test.cpp
+++ FtcRobotController/src/main/jni/test.cpp
@@ -19,37 +19,11 @@
 
 #include <jni.h>
 
-// #define reserveType(type, name) name, name##end = name + sizeof(type)
-// #define reserveBytes(n_bytes, name) name, name##size = (n_bytes), name##end = name + (n_bytes)-1
-// #define reserveArray(type, n_elements, name) name, name##size = sizeof(type)*(n_bytes), name##end = name + sizeof(type)*(n_bytes)-1
-
 struct gamepad
 {
     v2f left_stick;
 };
 #include "test_robot_state_elements.h"
-
-// enum robot_state_element
-// {
-//     reserveType(float, rsid_left_drive_power),
-//     reserveType(float, rsid_right_drive_power),
-//     reserveType(gamepad, rsid_gamepad1),
-//     //reserveBytes(100, rsid_array_example),
-//     rsid_size,
-// };
-
-// #undef reserve_int
-// #undef reserve_float
-
-// #define add_robot_state_element(type, name) type & name = ((type *) robot_state.state)[rsid_##name]
-// add_robot_state_element(float, left_drive_power);
-// add_robot_state_element(float, right_drive_power);
-    
-// add_robot_state_element(gamepad, gamepad1);
-
-//add_robot_state_element(char, array_example);
-
-// #undef add_robot_state_element
 
 extern "C"
 void JNI_main(JNIEnv * env, jobject self)
