bf8cf8cd19ab309eec3f37e084b156874913e8c6
Dev <dev.rangarajan@gmail.com>
2015-12-31 19:07:05 -0800

    Added Mk3Teleop and outlined it

---
+++ FtcRobotController/src/main/jni/Mk3Teleop.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Dev on 12/31/2015.
+//
+
+
+//============================= Drive ============================
+//TODO: Smoothing
+//TODO: Traction control
+//TODO: Negative Inertia
+//TODO: D-PAD Turn macros (90, 180, 270, simulated shift)
+//TODO: Drive on course (turn by bearing)
+//TODO: Stabilized driving (if we get slammed, we should correct)
+
+//============================== Arm =============================
+//TODO: Inverse Kinematics
+//TODO: Feed-Forward
+//TODO: Position macros (DPAD on second controller)
+//TODO: Precision mode
+//TODO: Rope tension
+//============================= Hopper ===========================
+//TODO: Intake
+//TODO: Auto-score
+//TODO: Block count
+//============================ Trigger ===========================
+//TODO: Single button sided extension (robot knows it's red or blue, extends accordingly)
+//TODO: Override so we can use the entire slide if we need to for some reason
